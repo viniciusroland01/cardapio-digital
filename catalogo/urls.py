dbed8f5d91b60c67803310346f1995bf427f0d59
viniciusroland01 <viniciusroland01@hotmail.com>
2026-04-27 21:00:17 -0300

    carrinho e finalizar pedido funcionando

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+# modelo = path('endereço/', views.funcao, name='apelido')
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('adicionar/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
-    path('carrinho/', views.ver_carrinho, name='ver_carrinho'),
+    path('', views.home, name='home'), #URL vazia, chama a função home apelido 'home'
+    path('adicionar/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'), #URL /adicionar/x/ chama a função adicionar_ao_carrinho 
+    path('carrinho/', views.ver_carrinho, name='ver_carrinho'), #URL /carrinho/ chama a função ver_carrinho
+    path('finalizar/', views.finalizar_pedido, name='finalizar_pedido'), #URL 'finalizar/', chama a função finalizar_pedido
 ]
